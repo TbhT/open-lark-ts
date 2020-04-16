2f447ced64633a93739147b972ca9e941c529833
TbhT <hustapple132@gmail.com>
2020-04-16 18:58:51 +0800

    feat: add oauth api

---
+++ src/Constants.ts
@@ -0,0 +1,19 @@
+export const UPLOAD_PATH = 'https://open.feishu.cn/open-apis/image/v4/put/'
+
+export const GET_IMAGE_PATH = 'https://open.feishu.cn/open-apis/image/v4/get'
+
+// *企业自建应用
+export const GET_APP_ACCESS_TOKEN =
+  'https://open.feishu.cn/open-apis/auth/v3/app_access_token/internal/'
+
+// *应用商店应用
+export const GET_APP_ACCESS_TOKEN_APP_STORE =
+  'https://open.feishu.cn/open-apis/auth/v3/app_access_token/'
+
+// *企业自建应用
+export const GET_TENANT_ACCESS_TOKEN =
+  'https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/internal/'
+
+// *应用商店应用
+export const GET_TENANT_ACCESS_TOKEN_APP_STORE =
+  'https://open.feishu.cn/open-apis/auth/v3/tenant_access_token/'
---
+++ src/OAuth.ts
@@ -0,0 +1,33 @@
+import axios, { AxiosInstance } from 'axios'
+import { GET_APP_ACCESS_TOKEN } from './Constants'
+import { AppAccessTokenResponse } from './types/Response'
+
+export async function getAppAccessToken({
+  appId,
+  appSecret,
+  instance
+}: {
+  instance?: AxiosInstance
+  appId: string
+  appSecret: string
+}): Promise<AppAccessTokenResponse> {
+  let $instance: AxiosInstance | undefined = instance
+
+  if (!$instance) {
+    $instance = axios.create({
+      headers: {
+        'content-type': 'application/json'
+      }
+    })
+  }
+
+  const { data } = await $instance.post<AppAccessTokenResponse>(
+    GET_APP_ACCESS_TOKEN,
+    {
+      app_id: appId,
+      app_secret: appSecret
+    }
+  )
+
+  return data
+}
