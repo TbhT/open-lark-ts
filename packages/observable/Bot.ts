cb9d4bfab45a356394ae3fada5288553d5d2e8f7
TbhT <hustapple132@gmail.com>
2021-01-18 20:35:56 +0800

    feat: add bot observables

--- packages/observable/Bot.ts
+++ packages/observable/Bot.ts
@@ -1,6 +1,10 @@
 import { RxHR } from '@akanass/rx-http-request'
 import { Observable } from 'rxjs'
-import { ADD_BOT_TO_CHAT, GET_BOT_INFO } from '../Constants'
+import {
+  ADD_BOT_TO_CHAT,
+  GET_BOT_INFO,
+  REMOVE_BOT_FROM_CHAT
+} from '../Constants'
 import { BotInfoResponse, CommonResponse } from '../../src/types/Response'
 
 const { get, post } = RxHR
@@ -18,10 +22,12 @@ export function getBotInfo({
     }).subscribe(
       data => {
         subscriber.next(data.body)
-        subscriber.complete()
       },
       error => {
         subscriber.error(error)
+      },
+      () => {
+        subscriber.complete()
       }
     )
   })
@@ -39,11 +45,43 @@ export function addBotToChat({
       headers: {
         Authorization: `Bearer ${tenantAccessToken}`
       },
-      json: true
+      json: true,
+      body: {
+        chat_id: chatId
+      }
     }).subscribe(
       data => {
         subscriber.next(data.body)
+      },
+      error => {
+        subscriber.error(error)
+      },
+      () => {
         subscriber.complete()
+      }
+    )
+  })
+}
+
+export function removeBotFromChat({
+  chatId,
+  tenantAccessToken
+}: {
+  chatId: string
+  tenantAccessToken: string
+}) {
+  return new Observable(subscriber => {
+    post<CommonResponse>(REMOVE_BOT_FROM_CHAT, {
+      headers: {
+        Authorization: `Bearer ${tenantAccessToken}`
+      },
+      json: true,
+      body: {
+        chat_id: chatId
+      }
+    }).subscribe(
+      data => {
+        subscriber.next(data.body)
       },
       error => {
         subscriber.error(error)
